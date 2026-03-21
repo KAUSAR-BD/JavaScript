8355817022b17a158d731b9e1d6791e668e0d59d
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-03-21 23:03:43 +0600

    Discussed about integer number

---
+++ number/number.js
@@ -0,0 +1,13 @@
+// Integer Number:- An integer is a whole number-- positive, negative, or zero-- without fractions or decimals. Examples:- ... -3, -2, -1, 0, 1, 2, 3....
+  
+
+
+// A decimal number includes a decimal point and can have a fractional part. Example:- 2.5, -0.75, 3.14
+
+//  A number like 5.0 is still an integer in value because it has no fractional part, even though it's written with a decimal point.
+// A number like 2.5 is not an integer because it has a non-zero digit afte the decimal.
+
+
+// We can say:- Not all decimals are integers, but all intergers can be written as decimals(5=5.0).
+
+
