2af377f6021e4109b1d0a46f504cb29474a72af8
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-03-22 22:12:50 +0600

    discussed about variable

---
+++ variable.js
@@ -0,0 +1,9 @@
+// A variable is a container for a value
+
+const weight = 60;
+const price = 10;
+const year = 1995;
+const balance = 1000;
+const age = 20;
+
+console.log(weight, price, year, balance, age);
