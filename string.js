cb7cf7c83382380f73f4c452ac80f180e77f3a9f
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-03-22 22:32:35 +0600

    discussed about string and template string

---
+++ string.js
@@ -0,0 +1,14 @@
+// Strings are for storing text
+// Strings are written with quotes
+
+// You can use single or double quotes
+const name = 'x';
+const text = "car";
+const price = '20'; // it is a string
+
+// Here is another string called Template String. It enclosed in backticks(`..`)
+const myName = `My Name is: "y"`;
+console.log(myName);
+
+// tamplate string introduced with es6(2016)
+//  
