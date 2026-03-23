133cddde5d8ae69b6c3467603bf9aac9c7774d62
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-03-23 12:02:09 +0600

    Boolean

---
+++ boolean.js
@@ -0,0 +1,29 @@
+// In JavaScript, boolean is a primitive data type that can only have one of two values: true or false.
+
+// true and false must be written in lowercase and without quotes
+
+const moneyMoved = true;
+console.log(moneyMoved);
+
+const learning = true;
+console.log(learning);
+
+const isChilling = true;
+console.log(isChilling);
+
+const homeWorkDone = false;
+console.log(homeWorkDone);
+
+const hasPhoneBattery = false;
+console.log(hasPhoneBattery);
+
+const isHappy = false;
+console.log(isHappy);
+
+const hasMoney = false;
+console.log(hasMoney);
+
+const isHungry = true;
+console.log(isHungry);
+
+
