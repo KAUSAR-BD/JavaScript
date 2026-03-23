a6eaa261bd1d18f55c3e5745f99136d212a674e9
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-03-23 14:08:51 +0600

    Addition Operation

---
+++ addition.js
@@ -0,0 +1,33 @@
+// Practice:- 1
+// A farmer has 4,567 apples in his first orchard, 2,389 apples in the second orchard, and 3,412 apples in the third orchard. How many apples does he have in total?
+
+const firstOrchardApple = 4567;
+const secondOrchardApple = 2389;
+const thirdOrchardApple = 3412;
+
+const totalApples = firstOrchardApple + secondOrchardApple + thirdOrchardApple;
+
+console.log("Total Apples:", totalApples);
+
+// Practice:- 2
+// A candy factory produced 12,345 candies in the morning, 9,876 candies in the afternoon, and 5,432 candies in the evening. How many candies were produced in total that day?
+
+const candiesMorning = 12345;
+const candiesAfternoon = 9876;
+const candiesEvening = 5432;
+
+const totalCandies = candiesMorning + candiesAfternoon + candiesEvening;
+
+console.log("Total Candies A Day:", totalCandies);
+
+// Practice:- 3
+// You read 450 pages in the first week, 380 pages in the second week, 620 pages in the third week, and 540 pages in the fourth week. How many pages did you read in total that month?
+
+const firstWeek = 450;
+const secondWeek = 380;
+const thirdWeek = 620;
+const fourthWeek = 540;
+
+const totalReadAMonth = firstWeek + secondWeek + thirdWeek + fourthWeek;
+
+console.log('Total Read A Month:', totalReadAMonth);
