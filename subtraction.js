8827efd0b6f5f97421d3ea577d1c3bbc3b99639c
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-03-23 14:40:05 +0600

    Subtraction

---
+++ subtraction.js
@@ -0,0 +1,27 @@
+// Problem:- 1
+// A factory produced 12,450 widgets this month. 7,835 widgets were shipped to clients. How many widgets remain in the factory?
+
+const productionWidget = 12450;
+const shippedWidget = 7835;
+
+const remainingWidget = productionWidget - shippedWidget;
+console.log("Widgets remaining in the factory:", remainingWidget);
+
+// Problem:- 2
+// A company had $125,000 in revenue last quarter. They spent $78,345 on expenses. How much money is left as profit?
+
+const lastQuarter = 125000;
+const expenses = 78345;
+
+const profit = lastQuarter - expenses;
+console.log("Total Profit: $", profit);
+
+// Problem:- 3
+// A global shipping company handled 987,650 parcels this week. 564,320 parcels were successfully delivered. How many parcels are still in their warehouses or trucks?
+
+const handledParcels = 987650;
+const parcelsDelivered = 564320;
+
+const remainingParcels = handledParcels - parcelsDelivered;
+console.log("Remaining parcels in warehouse:", remainingParcels);
+
