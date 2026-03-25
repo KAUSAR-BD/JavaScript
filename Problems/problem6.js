ed0ed7c04d042dcd58244c1c3bfb59cb32468d3f
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-03-25 22:34:46 +0600

    changed the varable name

--- Problems/problem6.js
+++ Problems/problem6.js
@@ -1,10 +1,10 @@
 // You have 45 apples and you want to divide them equally among 6 friends. After giving each friend the same number of apples, how many apples will be left? Write a program to calculate both the number each friend gets and the remainder using %.
 
-const Apples = 45;
-const Friends = 6;
+const totalApples = 45;
+const totalFriends = 6;
 
-const eachFriendsGets = Math.floor(Apples / Friends);
-const RemainingApples = Apples % Friends;
+const eachFriendsGets = Math.floor(totalApples / totalFriends);
+const RemainingApples = totalApples % totalFriends;
 
 console.log("Each friend gets apples:", eachFriendsGets);
 console.log("Remaining apples:", RemainingApples);
